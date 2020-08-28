7e4ebc69d586851030cb7ff7793584c920f5f6ad
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-08-28 18:06:54 +1000

    JDBC RowMapper

--- src/main/java/comin/minutes/database/databasedemo/SpringJdbcDemoApplication.java
+++ src/main/java/comin/minutes/database/databasedemo/SpringJdbcDemoApplication.java
@@ -12,14 +12,14 @@ import org.slf4j.LoggerFactory;
 import java.util.Date;
 
 @SpringBootApplication
-public class DatabaseDemoApplication  implements CommandLineRunner {
+public class SpringJdbcDemoApplication implements CommandLineRunner {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	PersonJdbcDAO dao;
 
 	public static void main(String[] args) {
-		SpringApplication.run(DatabaseDemoApplication.class, args);
+		SpringApplication.run(SpringJdbcDemoApplication.class, args);
 	}
 
 	@Override
--- src/main/java/comin/minutes/database/databasedemo/jdbc/PersonJdbcDAO.java
+++ src/main/java/comin/minutes/database/databasedemo/jdbc/PersonJdbcDAO.java
@@ -4,8 +4,11 @@ import comin.minutes.database.databasedemo.bean.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -15,29 +18,41 @@ public class PersonJdbcDAO {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    class PersonRowMapper implements RowMapper<Person> {
+        @Override
+        public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Person person = new Person();
+            person.setId(rs.getInt("id"));
+            person.setName(rs.getString("name"));
+            person.setLocation(rs.getString("location"));
+            person.setBirthday(rs.getTimestamp("birth_date"));
+            return person;
+        }
+
+    }
+
     public List<Person> findAll() {
-        return jdbcTemplate.query("select * from person",
-                new BeanPropertyRowMapper<>(Person.class));
+        return jdbcTemplate.query("select * from person", new PersonRowMapper());
     }
 
     public Person findById(int id) {
-        return jdbcTemplate.queryForObject("select * from person where id=?", new Object[]{id},
-                new BeanPropertyRowMapper<Person>(Person.class));
+        return jdbcTemplate.queryForObject("select * from person where id=?", new Object[] { id },
+                new BeanPropertyRowMapper<>(Person.class));
     }
 
     public int deleteById(int id) {
-        return jdbcTemplate.update("delete from person where id=?", new Object[]{id});
+        return jdbcTemplate.update("delete from person where id=?", new Object[] { id });
     }
 
     public int insert(Person person) {
         return jdbcTemplate.update("insert into person (id, name, location, birth_date) " + "values(?,  ?, ?, ?)",
-                new Object[]{person.getId(), person.getName(), person.getLocation(),
-                        new Timestamp(person.getBirthday().getTime())});
+                new Object[] { person.getId(), person.getName(), person.getLocation(),
+                        new Timestamp(person.getBirthday().getTime()) });
     }
 
     public int update(Person person) {
         return jdbcTemplate.update("update person " + " set name = ?, location = ?, birth_date = ? " + " where id = ?",
-                new Object[]{person.getName(), person.getLocation(), new Timestamp(person.getBirthday().getTime()),
-                        person.getId()});
+                new Object[] { person.getName(), person.getLocation(), new Timestamp(person.getBirthday().getTime()),
+                        person.getId() });
     }
 }
